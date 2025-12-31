8e6a52cfd4b8faed1ce73d52ffbcce498c6166a4
“KaiserinDerWelt” <desjmarianaguilar@gmail.com>
2025-12-31 18:21:59 -0300

    Write interface for Pagination

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,1 +1,9 @@
 // Pagination display in the bottom
+'use client';
+
+import type { PaginationInfo } from '@/types/movie';
+
+interface PaginationProps {
+  pagination: PaginationInfo | null;
+  onPageChange: (page: number) => void;
+}
