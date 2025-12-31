3c5c14d56bbabc4af84c872311100548517c7c22
“KaiserinDerWelt” <desjmarianaguilar@gmail.com>
2025-12-31 14:41:41 -0300

    Write search bar interface

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,1 +1,12 @@
-// This component was created by search for the movies.
+// This component was created by search for the movies, the user can search the movies manually.
+'use client';
+
+import { useState, useEffect } from 'react';
+
+interface SearchBarProps {
+  onSearch: (query: string) => void;
+  placeholder?: string;
+  initialValue?: string;
+}
+
+
