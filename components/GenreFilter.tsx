c0fd73d74fb758b8eb992d6093531a91b067ee01
“KaiserinDerWelt” <desjmarianaguilar@gmail.com>
2025-12-31 15:31:19 -0300

    Write interface for genre filter.

--- components/GenreFilter.tsx
+++ components/GenreFilter.tsx
@@ -1,1 +1,9 @@
 // The filter to show genre provided by the API Service
+'use client';
+
+import { getAllGenres } from '@/services/moviesAPI';
+
+interface GenreFilterProps {
+  selectedGenre?: string;
+  onGenreChange: (genre: string | undefined) => void;
+}
