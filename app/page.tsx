bece2855a5d34533428d3d6dd5eebcb2664b20e5
“KaiserinDerWelt” <desjmarianaguilar@gmail.com>
2025-12-31 19:28:50 -0300

    Add imports to the render page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import styles from "./page.module.css";
+//import { useState } from 'react';
+import { useMovies, useDebounce } from '@/hooks/theMovies';
+import SearchBar from '@/components/SearchBar';
+import GenreFilter from '@/components/GenreFilter';
+import MovieGrid from '@/components/MovieGrid';
+import Pagination from '@/components/Pagination';
+import ResultsCount from '@/components/ResultsCount';
 
 export default function Home() {
   return (
