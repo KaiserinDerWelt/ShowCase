b6430de1641443c06b892ef1056a60717711cf28
“KaiserinDerWelt” <desjmarianaguilar@gmail.com>
2025-12-31 18:45:57 -0300

    Write interface for the results count

--- components/ResultsCount.tsx
+++ components/ResultsCount.tsx
@@ -1,1 +1,9 @@
 //This component gonna handle the results count, remember in the API I've set a limit of 25 results per page.
+'use client';
+
+interface ResultsCountProps {
+  total: number;
+  searchQuery?: string;
+  genre?: string;
+}
+
